6b02e8d4363ad05e6ed62fc07e18187ca1a0e890
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2023-12-12 14:15:21 +0700

    change naming system and scaffold lab 2

---
+++ Labs/Lab2/2-6:adding-2-interger-list.py
@@ -0,0 +1,3 @@
+# กําหนดให้ list x และ y เป็น list ของจํานวนเต็ม โดยมีขนาดเท่ากัน
+# ให้ return list ที่เป็นผลบวกของ list x และ y โดยใช้ list comprehension
+# ให้เขียนในฟังก์ชัน function ชื่อ add2list(lst1,lst2)
